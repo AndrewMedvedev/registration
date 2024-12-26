767bbd6dd1604a61e620d7ea46046b4f4edee3e3
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2024-12-26 22:19:41 +0500

    fix

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ app.add_middleware(SlowAPIMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
