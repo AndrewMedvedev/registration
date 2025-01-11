baa4937c4558b94549e61c213301fb28899c8d54
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2025-01-12 03:31:49 +0500

    add vk

--- src/routers/validate_jwt.py
+++ src/routers/validate_jwt.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, HTTPException, status, Request, Response
 from src.database.controls import JWTControl, ValidateJWT
 
-router = APIRouter(prefix="/validate_jwt", tags=["validate_jwt"])
+router = APIRouter(prefix="/validate/jwt", tags=["validate/jwt"])
 
 
-@router.post("/validate_jwt_refresh")
+@router.post("/refresh")
 async def validate_refresh(refresh: str, response: Response):
     tkn_refresh = await ValidateJWT.validate_refresh(refresh)
     if tkn_refresh != False:
@@ -19,7 +19,7 @@ async def validate_refresh(refresh: str, response: Response):
         return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
 
-@router.get("/validate_jwt_access")
+@router.get("/access")
 async def validate_access(request: Request):
     access = request.cookies.get("access")
     tkn_access = await ValidateJWT.validate_access(access)
