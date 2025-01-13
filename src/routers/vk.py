6c94821f54d2cfeba6b2ed3628047777acd997a2
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2025-01-13 13:00:12 +0500

    add Annotated

--- src/routers/vk.py
+++ src/routers/vk.py
@@ -23,39 +23,38 @@ async def vk_link() -> str:
 
 
 @router.get("/registration")
-async def vk_registration(response: Response, code: str) -> dict:
-    try:
-        params = {
-            "client_secret": settings.CLIENT_SECRET,
-            "grant_type": "authorization_code",
-            "code_verifier": settings.CODE_VERIFIER,
-            "redirect_uri": settings.VK_REDIRECT_URI,
-            "code": code,
-            "client_id": settings.VK_APP_ID,
-            "state": settings.STATE,
-        }
-        user = await VKControls.get_token(params)
-        token_control = JWTControl()
-        user_model = UserVk(
-            id_vk=user.get("user_id"),
-            email=(user.get("email")).lower()
-        )
-        await ORMService().add_user(user_model)
-        data_tkn = {"user_name": user.get("user_id")}
-        access = await token_control.create_access(data_tkn)
-        refresh = await token_control.create_refresh(data_tkn)
-        response.set_cookie(
-        key="access", value=access, httponly=True, secure=True, samesite="none"
+async def vk_registration(response: Response, code: str):
+    params = {
+        "client_secret": settings.CLIENT_SECRET,
+        "grant_type": "authorization_code",
+        "code_verifier": settings.CODE_VERIFIER,
+        "redirect_uri": settings.VK_REDIRECT_URI,
+        "code": code,
+        "client_id": settings.VK_APP_ID,
+        "state": settings.STATE,
+    }
+    user = await VKControls.get_token(params)
+    token_control = JWTControl()
+    user_model = UserVk(
+        id_vk=user.get("user_id"),
+        email=(user.get("email")).lower()
     )
-        return {"refresh": refresh}
-    except:
-        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    await ORMService().add_user(user_model)
+    data_tkn = {"user_name": user.get("user_id")}
+    access = await token_control.create_access(data_tkn)
+    refresh = await token_control.create_refresh(data_tkn)
+    response.set_cookie(
+    key="access", value=access, httponly=True, secure=True, samesite="none"
+)
+    return {"refresh": refresh}
+
+        # return HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
 
         
 
 
 @router.get("/login")
-async def vk_login(response: Response, code: str) -> dict:
+async def vk_login(response: Response, code: str):
     params = {
         "client_secret": settings.CLIENT_SECRET,
         "grant_type": "authorization_code",
