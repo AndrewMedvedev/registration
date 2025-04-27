bcd40593748f808ba6310b0c0c95d6edb2a4fa9e
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2025-04-27 17:23:22 +0500

    Исправил небольшую ошибку при получении данных

--- src/routers/get_data.py
+++ src/routers/get_data.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 
@@ -9,7 +11,7 @@ data = APIRouter(prefix=f"{PATH_ENDPOINT}get", tags=["get_data"])
 
 @data.get("data/{user_id}")
 async def get_data(
-    user_id: int,
+    user_id: UUID,
 ) -> JSONResponse:
     content = await GetUserDataControl().get_data(user_id=user_id)
     return JSONResponse(status_code=status.HTTP_200_OK, content=content)
